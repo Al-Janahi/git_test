94a5c612646520f8266777cce6d61af9fa313b58
Al-Janahi <aljana7i.ai@gmail.com>
2026-09-13 12:48:54 +0300

    project finished

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,9 +1,26 @@
 const container = document.querySelector("#container");
+const gridButton = document.querySelector("#gridButton");
+let N = 16;
+let boxes;
+function createGrid(N) {
+    container.innerHTML = ""
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            container.appendChild(square);
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        container.appendChild(square);
+            square.addEventListener("mouseenter", () => {
+                square.style.backgroundColor = "black";
+            });
+
+            square.style.width = `${640 / N}px`;
+            square.style.height = `${640 / N}px`;
+        }
     }
 }
+createGrid(N);
+gridButton.addEventListener("click", () => {
+    N = prompt("choose a number");
+    createGrid(N);
+})
